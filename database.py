cc5e424a4684f5755599598829a2a08bbda60784
Red <vanishshovel@proton.me>
2026-04-08 18:52:29 +1000

    database connection

--- database.py
+++ database.py
@@ -1,4 +1,10 @@
-entries = []
+import sqlite3
+
+connection = sqlite3.connect("data.db")
+
+def create_table():
+    with connection:
+        connection.execute("CREATE TABLE entries(content TEXT, date TEXT)")
 
 def add_entry(content, date):
     entries.append({"content": content, "date": date})
